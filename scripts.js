1ae28b8d9e9719041f5cd4a22db2ae969afada75
ruhancmendes <ruhancmendes@gmail.com>
2025-02-28 17:15:29 -0300

    format value to BRL

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Seleciona os elementos do formulário
+const amount = document.getElementById("amount")
+
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+    // Obtém o valor do input, replace substitui caracteres que não são numéricos.
+    let value = amount.value.replace(/\D/g, "")
+
+    // Transforma o valor em centavos (ex. 150/100 = 1.5 que é equivalente a R$1,50)
+    value = Number(value) / 100
+
+    //retorna o value apenas com números
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    //Retorna o valor formatado.
+    return value
+}
